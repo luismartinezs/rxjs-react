72e84c5dd7f54b16d05fcdcac48f02b9056e5813
Luis Martinez <luismartinezwebdev@gmail.com>
2022-10-01 11:37:31 +0200

    feat: toggle labels and keep the association

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
 import { useA11y } from "../utils/a11y";
+import { Input } from "./Input";
+import { Label } from "./Label";
 
 export const Form: React.FC<{}> = () => {
-  const [show, toggle] = useState(true);
-  const { useRegisteredId, useLabelledOrDescribedBy } = useA11y();
+  const [showName, setShowName] = useState(true);
+  const [showEmail, setShowEmail] = useState(true);
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
   return (
     <div>
-      <button onClick={() => toggle(!show)}>Toggle input</button>
+      <button onClick={() => setShowName(!showName)}>Toggle name</button>
+      <button onClick={() => setShowEmail(!showEmail)}>Toggle email</button>
       <form onSubmit={handleSubmit}>
-        {show && <label id={useRegisteredId("name")}>Name</label>}
-        <input
-          type="text"
-          id="name"
-          aria-labelledby={useLabelledOrDescribedBy("name")}
-        />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-        <button type="submit">Submit</button>
+        {showName && <Label name="name" label="Name" />}
+        <Input name="name" />
+        {showEmail && <Label name="email" label="Email" />}
+        <Input name="email" />
       </form>
     </div>
   );
--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useA11y } from "../utils/a11y";
 
-export const Label: React.FC<{ key: string; label: string }> = ({
-  key,
+export const Label: React.FC<{ name: string; label: string }> = ({
+  name,
   label,
 }) => {
-  const id = useA11y().useRegisteredId(key);
+  const id = useA11y().useRegisteredId(name);
   return <span id={id}>{label}</span>;
 };
