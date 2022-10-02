f9738de6c67e85ac9b8eb736db451c282b864827
Luis Martinez <luismartinezwebdev@gmail.com>
2022-10-02 19:10:26 +0200

    chore: delete unused files

--- src/components/_old/Input.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useA11y } from "../utils/a11y";
-
-export const Input: React.FC<{ name: string }> = ({ name }) => {
-  const labelledBy = useA11y().useLabelledOrDescribedBy(name);
-  return <input type="text" aria-labelledby={labelledBy} />;
-};
