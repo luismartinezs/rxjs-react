3838e2189d27699f139c92b5971509f293de7366
Luis Martinez <luismartinezwebdev@gmail.com>
2022-10-02 22:48:35 +0200

    feat: limit modal count

--- src/examples/top-modal/Modal.tsx
+++ src/examples/top-modal/Modal.tsx
@@ -1,7 +1,7 @@
 export default function Modal({ onClose, children }) {
   return (
-    <div className="absolute w-32 h-32 bg-zinc-200 rounded-lg">
-      {children}
+    <div className="absolute w-32 h-32 bg-zinc-200 rounded-lg flex flex-col justify-between p-2">
+      <div className="text-xl font-bold my-2">{children}</div>
       <button onClick={onClose}>Close</button>
     </div>
   );
--- src/examples/top-modal/Root.tsx
+++ src/examples/top-modal/Root.tsx
@@ -6,7 +6,19 @@ export default function Root() {
 
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Open new modal</button>
+      <button
+        onClick={() =>
+          setCount((count) => {
+            if (count >= 5) {
+              return count;
+            }
+            return count + 1;
+          })
+        }
+      >
+        Open new modal
+      </button>
+      <div className="text-sm">5 levels allowed</div>
       <div className="relative">
         {count > 0 && (
           <Modal onClose={() => setCount(count - 1)}>{count}</Modal>
