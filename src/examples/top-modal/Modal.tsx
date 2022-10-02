054b90e83b53aa10655ea21d5e2830745897ea0a
Luis Martinez <luismartinezwebdev@gmail.com>
2022-10-02 21:48:09 +0200

    feat: shitty fake modals

--- src/examples/top-modal/Modal.tsx
+++ src/examples/top-modal/Modal.tsx
@@ -1,3 +1,8 @@
-export default function Modal() {
-  return <div>Modal</div>;
+export default function Modal({ onClose, children }) {
+  return (
+    <div className="absolute w-32 h-32 bg-zinc-200 rounded-lg">
+      {children}
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
 }
--- src/examples/top-modal/Root.tsx
+++ src/examples/top-modal/Root.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Modal from "./Modal";
 
 export default function Root() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <Modal />
-    </div>
+    <>
+      <button onClick={() => setCount(count + 1)}>Open new modal</button>
+      <div className="relative">
+        {count > 0 && (
+          <Modal onClose={() => setCount(count - 1)}>{count}</Modal>
+        )}
+        {count > 1 && (
+          <Modal onClose={() => setCount(count - 1)}>{count}</Modal>
+        )}
+        {count > 2 && (
+          <Modal onClose={() => setCount(count - 1)}>{count}</Modal>
+        )}
+        {count > 3 && (
+          <Modal onClose={() => setCount(count - 1)}>{count}</Modal>
+        )}
+        {count > 4 && (
+          <Modal onClose={() => setCount(count - 1)}>{count}</Modal>
+        )}
+        {count > 5 && (
+          <Modal onClose={() => setCount(count - 1)}>{count}</Modal>
+        )}
+      </div>
+    </>
   );
 }
