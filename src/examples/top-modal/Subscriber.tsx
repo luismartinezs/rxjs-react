03c882071939e12ada8d9b4b6c74b5683eb65498
Luis Martinez <luismartinezwebdev@gmail.com>
2022-10-03 10:57:28 +0200

    feat: make title accessible

--- src/examples/top-modal/Subscriber.tsx
+++ src/examples/top-modal/Subscriber.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { createRef, useEffect } from "react";
 import { useStore } from "./store";
 
 const Subscriber = () => {
+  const valueRef = createRef<HTMLParagraphElement>();
+
   useStore().useAdd(0, "Root");
-  return <div>{useStore().useSubscribe() || "Root"}</div>;
+  const value = useStore().useSubscribe() || "Root";
+
+  useEffect(() => {
+    const oldTitle = document.title;
+    document.title = value;
+    return () => {
+      document.title = oldTitle;
+    };
+  }, [value]);
+
+  useEffect(() => {
+    if (valueRef.current) {
+      valueRef.current.focus();
+    }
+  }, [value, valueRef]);
+
+  return (
+    <span tabIndex={-1} ref={valueRef} className="sr-only">
+      {value}
+    </span>
+  );
 };
 
 export default Subscriber;
